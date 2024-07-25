f3edd4cdb0baa9fd431d02b2329d48e1ac6596af
richterma89440 <richterma89440@th-nuernberg.de>
2024-07-26 00:53:07 +0200

    1.0

--- tello_dronecontrol.py
+++ tello_dronecontrol.py
@@ -9,51 +9,60 @@ from djitellopy import Tello
 import time
 
 class TelloDroneController:
+    
+        
     def __init__(self):
         self.drone = Tello()
         print("Drone initialized.")
+        
+        # Initialisiere Geschwindigkeiten
         self.speed_left_right = 0  # Links/Rechts
         self.speed_up_down = 0     # Auf/Ab
         self.speed_forward_back = 0  # Vorwärts/Rückwärts
-        self.yaw_speed = 0         # Yaw (Drehung)
+        self.yaw_speed = 0  # Yaw (Drehung)
+        
         self.drone.connect()
         print("Drone connected.")
         self.drone.streamoff()
         self.drone.streamon()
-        self.takeoff()  # Drohne hebt ab, sobald die Instanz erstellt wird
-        # Initialisiere die Geschwindigkeiten für alle Richtungen auf 0
         
-
+        self.takeoff() # Drohne hebt ab, sobald die Instanz erstellt wird
+        
+        self.speed = 30 # Geschwindigkeit der Drohne
+        
+        
+    
+    
     def update_movement(self):
         self.drone.send_rc_control(self.speed_left_right, self.speed_forward_back, self.speed_up_down, self.yaw_speed)
 
     def up(self):
-        self.speed_up_down = 30
+        self.speed_up_down = self.speed
         self.update_movement()
         print("Tello: Up")
 
     def down(self):
-        self.speed_up_down = -30
+        self.speed_up_down = -self.speed
         self.update_movement()
         print("Tello: Down")
 
     def left(self):
-        self.speed_left_right = -30
+        self.speed_left_right = -self.speed
         self.update_movement()
         print("Tello: Left")
 
     def right(self):
-        self.speed_left_right = 30
+        self.speed_left_right = self.speed
         self.update_movement()
         print("Tello: Right")
 
     def forward(self):
-        self.speed_forward_back = 30
+        self.speed_forward_back = self.speed
         self.update_movement()
         print("Tello: Forward")
 
     def backward(self):
-        self.speed_forward_back = -30
+        self.speed_forward_back = -self.speed
         self.update_movement()
         print("Tello: Backward")
 
